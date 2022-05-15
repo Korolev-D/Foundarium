35001035aa77070cef45a9ea35604fee21c2aa13
korolev <denis.korolev@lego-car.ru>
2022-05-16 00:52:34 +0500

    add test for Car

--- tests/Feature/CarTest.php
+++ tests/Feature/CarTest.php
@@ -2,21 +2,118 @@
 
 namespace Tests\Feature;
 
+use App\Models\Car;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class CarTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function clearTestCar()
     {
-        $response = $this->get('/');
+        $carIdForDelete = Car::where('model', 'AUDI')->value('id');
+        $this->json('delete', "api/cars/$carIdForDelete");
+    }
 
-        $response->assertStatus(200);
+    public function testGetCars()
+    {
+        $this->json('get', 'api/cars')
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure(
+                [
+                    'data' => [
+                        '*' => [
+                            'id',
+                            'model',
+                            'created_at',
+                        ]
+                    ]
+                ]
+            );
+    }
+
+    public function testGetCar()
+    {
+        $this->clearTestCar();
+
+        $carData = [
+            'model' => 'AUDI',
+        ];
+
+        $car = Car::create(
+            $carData
+        );
+
+        $this->json('get', 'api/cars', ['id' => $car->id])
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure(
+                [
+                    'data' => [
+                        '*' => [
+                            'id',
+                            'model',
+                            'created_at',
+                        ]
+                    ]
+                ]
+            );
+
+        $this->assertDatabaseHas('cars', $carData);
+    }
+
+    public function testCreateCar()
+    {
+        $this->clearTestCar();
+
+        $carData = [
+            'model' => 'AUDI',
+        ];
+
+        $this->json('post', 'api/cars', $carData)
+            ->assertStatus(Response::HTTP_CREATED)
+            ->assertJsonStructure(
+                [
+                    'data' => [
+                        'id',
+                        'model',
+                        'created_at',
+                    ]
+                ]
+            );
+        $this->assertDatabaseHas('cars', $carData);
+    }
+
+    public function testUpdateCar()
+    {
+        $this->clearTestCar();
+
+        $car = Car::create([
+            'model' => 'AUDI',
+        ]);
+
+        $carUpdate = [
+            'model' => 'BMW',
+        ];
+
+        $this->json('put', "api/cars/$car->id", $carUpdate)
+            ->assertStatus(Response::HTTP_OK);
+    }
+
+    public function testDeleteCar()
+    {
+        $this->clearTestCar();
+
+        $carData = [
+            'model' => 'AUDI',
+        ];
+
+        $car = Car::create(
+            $carData
+        );
+
+        $this->json('delete', "api/cars/$car->id")
+            ->assertNoContent();
+        $this->assertDatabaseMissing('cars', $carData);
     }
 }
